1bc581f33383bb7eb78a691084ed83847a4fb24e
rizxddev <rizz994564@gmail.com>
2025-07-20 22:12:02 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    fetch('/public/guru.json')
+    fetch('/guru.json')
       .then(r => r.json())
       .then(json => setGuruList(json.guru || []));
   }, []);
