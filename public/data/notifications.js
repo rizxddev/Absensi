cf07dcb396b1229f72c9baa234d82a50b9de19d4
rizxddev <rizz994564@gmail.com>
2026-01-19 15:01:39 +0700

    Create notifications.js

---
+++ public/data/notifications.js
@@ -0,0 +1,8 @@
+{
+  "id": "notifications",
+  "copy_success": "Hasil absensi berhasil disalin ke clipboard!",
+  "save_success": "Data berhasil disimpan!",
+  "error": "Terjadi kesalahan",
+  "last_updated": "2025-01-15T10:30:00.000Z",
+  "version": 2
+}
